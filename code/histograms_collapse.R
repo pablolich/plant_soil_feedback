6ae45368dc7cfff708f160852bc701a18aa5356b
Zach Miller <zachmiller@uchicago.edu>
2021-10-22 14:40:31 -0500

    Updated histograms code and figure

--- code/histograms_collapse.R
+++ code/histograms_collapse.R
@@ -1,64 +1,50 @@
 library(tidyverse)
-library(scale)
-library(gridExtra)
-library(cowplot)
-setwd("~/Desktop/phd/plant_soil_feedback/code/")
-data = read.csv('../data/results.csv', row.names = 1)
-dev.off()
-#Remove non-convergent data
-data = data %>% filter(n_p != -1)
-#Get number of different initial species
-n_init_vec = unique(data$n_p)
-#Get total number of simulations
-n_sim = max(data$n_sim)
-#Initialize list of plots
-myplots = list()
-#Plot histograms for each value of n_init_vec
-for (i in seq(length(n_init_vec))){
-  #Prepare data
-  data_i = data %>% 
-    filter(n_p == n_init_vec[i]) %>% 
-    gather(key = 'n_sp', value = 'count', -n_sim)
-  #Generate plot
-  p_i = ggplot(data = data_i, aes(x = count, 
-                                  fill = n_sp, 
-                                  color = n_sp,
-                                  linetype = n_sp))+ 
-    geom_histogram(position = 'identity', 
-                   alpha = 0.5,
-                   binwidth = 1)+
-    #coord_cartesian(xlim = c(1.5, 6.5))+
-    theme(axis.title = element_blank(),
-          axis.text = element_text(size = 15),
-          legend.position = 'top',
-          legend.title = element_text(size = 14),
-          legend.text = element_text(size = 12, hjust = 0.1),
-          legend.title.align = 0.5,
-          legend.box.margin = margin(b = -50),
-          panel.background = element_blank(), 
-          panel.border = element_rect(color = 'black', fill=NA),
-          aspect.ratio = 0.5)+
-    scale_fill_manual(values = c('red', 'blue'),
-                      labels = c('Before', 'After'),
-                      guide = guide_legend(direction = "horizontal",
-                                           title.position = "top"))+
-    scale_color_manual(values = c('red', 'blue'),
-                       labels = c('Before', 'After'))+
-    scale_linetype_manual(values = c('solid', 'dashed'),
-                          labels = c('Before', 'After'))+
-    scale_x_continuous(breaks = n_init_vec,
-                       limits = c(1.5, 6.5))+
-    scale_y_continuous(breaks = seq(0, n_sim + 1, length.out = n_sim/3),
-                       limits = c(0, n_sim + 1),
-                       expand = expansion(mult = c(0, .1)))+
-    labs(fill = 'Number of coexisting species',
-         color = 'Number of coexisting species',
-         linetype = 'Number of coexisting species')
-  legend <- get_legend(p_i)
-  #Store
-  myplots[[i]] =  p_i + theme(legend.position = "none")
-}
 
-grid.arrange(legend, myplots[[1]], myplots[[2]], myplots[[3]], myplots[[4]], nrow = length(n_init_vec) + 1)
+data = read_csv('../data/results.csv')
+n_sim = round(max(data$n_sim), digits = -2) # number of simulations per inital richness
+
+# Remove non-convergent data (need to filter on n_p_f, rather than n_p)
+# Hopefully we can deal with these cases in the simulation code and eventually
+# remove this line
+data = data %>% filter(n_p_f != -1)
+
+# Reshape data for easier ggplotting
+data = data %>% 
+  mutate(n_p_s = n_p) %>%
+  pivot_longer(cols = c(n_p_s, n_p_f),
+               names_to = "type",
+               values_to = "num_species")
+
+data %>% ggplot(aes(x = num_species,
+                    after_stat(density),
+                    fill = type,
+                    color = type,
+                    linetype = type)) + 
+  geom_histogram(position = "identity",
+                 alpha = 0.5,
+                 binwidth = 1) +
+  facet_wrap(.~n_p, scales = "free") + 
+  theme_classic() + 
+  theme(axis.title = element_text(size = 15),
+         axis.text = element_text(size = 15),
+         strip.text = element_text(size = 15),
+         legend.title = element_blank(),
+         legend.position = c(0.35, 0.85),
+         legend.text = element_text(size = 12, hjust = 0.1),
+         panel.background = element_blank(), 
+         panel.border = element_rect(color = 'black', fill = NA), 
+         aspect.ratio = 0.5) + 
+  scale_fill_manual(values = c('red', 'blue'),
+                    labels = c('Final', 'Initial')) + 
+  scale_color_manual(values = c('red', 'blue'),
+                     labels = c('Final', 'Initial'))+
+  scale_linetype_manual(values = c('solid', 'dashed'),
+                        labels = c('Final', 'Initial'))+
+  scale_x_continuous(breaks = seq(1:max(data$num_species)),
+                     limits = c(0.5, max(data$num_species) + 0.5)) +
+  scale_y_continuous(breaks = c(0, 0.5, 1),
+                     expand = expansion(mult = c(0, 0.1))) + 
+  xlab("Number of species") + 
+  ylab("Proportion of communities")
+
 ggsave('../sandbox/histogram_collapse.pdf')
-
