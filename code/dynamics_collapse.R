e9cee0e3fd61dc9825ca8f6acfc26ac9f17eb885
Pablo Lechon <plechon@ucm.es>
2021-10-26 08:00:02 -0500

    some  edits to figure 2

--- code/dynamics_collapse.R
+++ code/dynamics_collapse.R
@@ -3,6 +3,7 @@ library(tidyquant)
 library(reshape2)
 library(cowplot)
 library(gridExtra)
+library(RColorBrewer)
 
 
 data = read_csv('../data/zach_results.csv')
@@ -12,7 +13,7 @@ p1 = ggplot(data = data,
            color = as.factor(p_l)))+
   geom_line(alpha = 0.2)+
   geom_ma(ma_fun = EMA, n = 150, linetype = 'solid')+
-theme(aspect.ratio = 3/4,
+  theme(aspect.ratio = 3/4,
             panel.background = element_blank(),
             panel.border = element_rect(fill = NA),
             legend.position = 'none',
@@ -20,6 +21,7 @@ theme(aspect.ratio = 3/4,
             axis.title.y = element_text(size = 15),
             axis.text = element_blank(),
             axis.ticks = element_blank())+
+  scale_color_brewer(palette = 'Set1')+
   labs(y = "Abundance",
        x = "Time")
 print(p1)
@@ -37,6 +39,7 @@ p2 = ggplot(data = data,
         axis.title.y = element_text(size = 15),
         axis.text = element_blank(),
         axis.ticks = element_blank())+
+  scale_color_brewer(palette = 'Set1')+
   labs(y = "",
        x = "Time")
 print(p2)
@@ -52,13 +55,21 @@ A_pre_plot =  ggplot(data = A_pre_melt,
   geom_tile()+
   theme(panel.background = element_rect(fill = NA),
         plot.background = element_blank(),
-        legend.position = 'none',
+        # legend.title = element_text(size = 15, hjust = 0.2,
+        #                             margin = margin(b = -10)),
+        legend.title = element_blank(),
+        legend.position = c(1.16, 0.55),
+        legend.background = element_rect(fill = NA),
         axis.text = element_blank(),
         axis.ticks = element_blank(),
         axis.title = element_blank(),
         aspect.ratio = 1)+
-    labs(x = '', y = '')
-
+  labs(x = '', y = '',
+       fill = expression(A[ij]))+
+  scale_fill_distiller(palette = 'RdYlBu',
+                       guide = guide_colorbar(barwidth = 0.6,
+                                              barheight = 4.5),
+                       breaks = c(0.5, 0.9, 1.3))
 print(A_pre_plot)
 
 A_post = my_list$A_post
@@ -69,17 +80,29 @@ A_post_plot =  ggplot(data = A_post_melt,
   geom_tile()+
   theme(panel.background = element_rect(fill = NA),
         plot.background = element_blank(),
-        legend.position = 'none',
+        legend.position = c(1.16, 0.55),
+        legend.background = element_rect(fill = NA),
+        # legend.title = element_text(size = 15, hjust = 0.2,
+        #                             margin = margin(b = -10)),
+        legend.title = element_blank(),
         axis.text = element_blank(),
         axis.ticks = element_blank(),
         axis.title = element_blank(),
         aspect.ratio = 1)+
-  labs(x = '', y = '')
+  labs(x = '', y = '',
+       fill = expression(A[ij]))+
+  scale_fill_distiller(palette = 'RdYlBu',
+                       guide = guide_colorbar(barwidth = 0.6,
+                                              barheight = 4.5),
+                       breaks = c(0.5, 0.9, 1.3))
+  
 
 print(A_post_plot)
 
 
 data = data.frame(x = as.vector(A_pre), y = as.vector(A_post))
+
+r_2 = paste("R^2 == ", round(summary(lm(data$y~data$x))$r.squared, digits = 3))
 
 correlation_plot = ggplot(data = data,
                           aes(x = x, y = y))+
@@ -92,6 +115,11 @@ correlation_plot = ggplot(data = data,
         axis.title.x = element_text(size = 15),
         axis.title.y = element_text(size = 15),
         axis.text = element_text(size = 10))+
+  scale_x_continuous(breaks = c(0.6, 0.9, 1.2, 1.5),
+                     limits = c(0.4, 1.5))+
+  scale_y_continuous(breaks = c(0.6, 0.9, 1.2, 1.5),
+                     limits = c(0.4, 1.5))+
+  annotate('text', x = 0.6, y = 1.4, label = r_2, parse = T)+
   labs(x = 'A', y = expression(A[p]))
   
 print(correlation_plot)
